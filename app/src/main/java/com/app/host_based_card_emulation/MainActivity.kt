70f7696f66b607c8bb71ae6e9b655a78dd1a06cc
Mykhailo Nester <nsmisha.dev@gmail.com>
2022-05-09 18:01:06 +0300

    - init commit;

---
+++ app/src/main/java/com/app/host_based_card_emulation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.app.host_based_card_emulation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
